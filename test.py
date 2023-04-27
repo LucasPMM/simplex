bb43dc7ee62066f383cb1229ba3ecac368012149
Lucas Mariz <lucaspaulom@hotmail.com>
2023-04-26 21:00:54 -0300

    fix: test script

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import os
 
-path_input = './tests/in'
-path_output = './tests/in'
+path_input = './tests/in/'
+path_output = './tests/out/'
 num_files = len([f for f in os.listdir(path_input) if os.path.isfile(os.path.join(path_input, f))])
 
 for i in range(num_files):
-    entradas = f'{path_input}in/t{i+1}_in.txt'
-    saidas = f'{path_output}out/t{i+1}_out.txt'
+    entradas = f'{path_input}t{i+1}_in.txt'
+    saidas = f'{path_output}t{i+1}_out.txt'
     os.system(f"python3 simplex.py {entradas} {saidas}")
